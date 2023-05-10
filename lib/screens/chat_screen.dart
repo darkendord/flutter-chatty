128e1104894ceddf5feeed6b38011de806798e18
darkendord <darkendord6@gmail.com>
2023-05-10 00:34:32 -0400

    feat: messages can be retrieved
    - Messages can be retrieved from data base and are displayed on real time.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -15,9 +15,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
   User? loggedInUser;
   String? messageText;
-
   @override
   void initState() {
     getCurrentUser();
@@ -35,6 +35,21 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  // void getMessages() async {
+  //   final messages = await _firestore.collection("messages").get();
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection("messages").snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,11 +57,13 @@ class _ChatScreenState extends State<ChatScreen> {
         leading: null,
         actions: <Widget>[
           IconButton(
-              icon: const Icon(Icons.close),
-              onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
-              }),
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              messagesStream();
+              // _auth.signOut();
+              // Navigator.pop(context);
+            },
+          ),
         ],
         title: const Text('⚡️Chat'),
         backgroundColor: Colors.lightBlueAccent,
@@ -56,6 +73,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            _messageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -63,6 +81,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -71,6 +90,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   TextButton(
                     onPressed: () {
+                      messageTextController.clear();
                       _firestore.collection("messages").add(
                         {
                           "text": messageText,
@@ -88,6 +108,105 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+Widget _messageStream() {
+  return StreamBuilder(
+    stream: _firestore.collection("messages").snapshots(),
+    builder: (context, snapshot) {
+      List<MessageBuble> messageBubles = [];
+
+      if (snapshot.hasError) {
+        print(snapshot.error);
+        return const Text("Something went wrong");
+      } else if (snapshot.hasData) {
+        final messages = snapshot.data?.docs;
+
+        for (var message in messages!) {
+          final messageText = message.data()["text"];
+          final messageSender = message.data()["sender"];
+
+          final messageBuble = MessageBuble(
+            sender: messageSender,
+            text: messageText,
+          );
+
+          messageBubles.add(messageBuble);
+        }
+      }
+
+      switch (snapshot.connectionState) {
+        case ConnectionState.waiting:
+        case ConnectionState.none:
+          return const LinearProgressIndicator();
+        case ConnectionState.active:
+          return Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 10,
+              ),
+              itemCount: messageBubles.length,
+              itemBuilder: (context, index) {
+                return messageBubles[index];
+              },
+            ),
+          );
+        case ConnectionState.done:
+          return Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 10,
+              ),
+              itemCount: messageBubles.length,
+              itemBuilder: (context, index) {
+                return messageBubles[index];
+              },
+            ),
+          );
+      }
+    },
+  );
+}
+
+class MessageBuble extends StatelessWidget {
+  const MessageBuble({Key? key, required this.sender, required this.text})
+      : super(key: key);
+
+  final String sender;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            sender,
+            style: const TextStyle(fontSize: 12, color: Colors.black54),
+          ),
+          Material(
+            borderRadius: BorderRadius.circular(30),
+            elevation: 5,
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text(
+                "$text from $sender",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
